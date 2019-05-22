40c5718113310bcb5828781f79d3c8662137c9e9
duzhengjun <870426133@qq.com>
2019-05-22 13:41:11 +0800

    一些练习

--- practice_after_class/chapter_10.py
+++ practice_after_class/chapter_10.py
@@ -6,7 +6,34 @@
 @detail:
 @else: Don't stop learning!!!
 """
-# 1、请输人一周中某天的名称的第一个英文字母来判断一下是星期几，如果第一个字母一样，则继续判断第二个字母。
+# 1、请输入一周中某天的名称的第一个英文字母来判断一下是星期几，如果第一个字母一样，则继续判断第二个字母。
+def juge(num,week_list):
+
+    w = input('请输入第%s个字母:'%num)
+    w = w.lower()
+    res = []
+    state = 0
+    for week in week_list:
+        if w == week[0 + num - 1: 1 + num - 1]:
+            state += 1
+            res.append(week)
+
+    if state == 1:
+        print('是%s'% res[0])
+    elif state > 1:
+        print('还要输入一次')
+        num += 1
+        week_list = res
+        juge(num, week_list)
+    else:
+        print('非法输入')
+
+
+if __name__ == '__main__':
+    #  初始条件
+    num = 1
+    week_list = ['mon', 'tues', 'wed', 'thur', 'fri', 'sat', 'sun']
+    juge(num, week_list)
 
 # 2、按相反的顺序输出列表的值。
 

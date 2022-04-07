73de560bf0ea7fdf42ba4174f9d8341bc6fae540
dududuzhengjun <48872757+dududuzhengjun@users.noreply.github.com>
2022-04-07 11:06:45 +0800

    Add files via upload

---
+++ make_test_data/ez6_web/add_team_relationship.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2022/4/7/ 10:48
+# @Author : duzhengjun
+# @File : add_team_relationship.py
+
+#导入请求包
+import requests
+import http.client
+import hashlib
+import urllib.parse
+import random
+from flask import Flask
+
